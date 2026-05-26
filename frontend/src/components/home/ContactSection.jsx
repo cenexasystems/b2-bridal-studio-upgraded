f36b64e113ee90838315d23b7390a2f70b5d928d
Priyanka-S-A <240288.cb@rmkec.ac.in>
2026-05-26 21:08:55 +0530

    mail chnage

--- frontend/src/components/home/ContactSection.jsx
+++ frontend/src/components/home/ContactSection.jsx
@@ -258,7 +258,7 @@ const ContactSection = () => {
                 {
                   icon: <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#FFD700" strokeWidth="1.5"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg>,
                   label: 'Email',
-                  value: 'b2shammu@gmail.com',
+                  value: 'b2bridalstudio@gmail.com',
                 },
               ].map((item, i) => (
                 <div
